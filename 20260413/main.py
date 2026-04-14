a498447458786491f4ab9fe3f9434ca54e4841fa
tony <ttoonnyy9@gmail.com>
2026-04-14 13:40:59 +0800

    20260413

---
+++ 20260413/main.py
@@ -0,0 +1,11 @@
+def main():
+    print("Hello from ps!")
+    print("大家好"+"\n我是鄭仕遠"+"\n今年30歲")
+    print("參加課程如下："+"Python課程:",12000,"元"+"JAVA課程:",10000,"元")
+    print("計算方式:"+"12000+10000")
+    print("小計=",12000+10000)
+    print("AAA"+"BBB")
+
+
+if __name__ == "__main__":
+    main()
